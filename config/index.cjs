97b028a33786713bb6d03a5549d81dd6306380ec
X1ao1u <106831271+X1ao1u@users.noreply.github.com>
2022-11-10 08:10:51 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -23,7 +23,7 @@ const USER_CONFIG = {
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'o6Wy_6pSCIi9cfwRYuQFCg5vyiXk',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: '1ZkoE7T4Qu4qCzEkhISp5XmC4S1vk6hwemRKgObYp3Y',
+      useTemplateId: 'dWI9xrk3sGFyY2V2k91wnwMwJ35zpIUn__XcCLXSeRk',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '10-01',
       festivals: [
